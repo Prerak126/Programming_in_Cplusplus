e903d5ed301dce3708242844ca38a1eefb45195a
Prerak126 <prerak126@gmail.com>
2016-11-01 23:23:16 -0400

    Factorial find upto 10^14 Digit

---
+++ Factorial.cpp
@@ -0,0 +1,19 @@
+#include<iostream> 
+using namespace std;
+int main() 
+{
+    int factorial_10=1; 
+	for(int n=10;1<=n;n--) 
+	    factorial_10*=n;
+   
+
+float factorial_20=1;
+ for(float a=20;1<=a;a--) 
+ factorial_20*=a;
+ 
+float factorial_30=1;
+ for(float z=30;1<=z;z--)
+  factorial_30*=z;
+  
+cout<<"10!= "<< factorial_10<<'\n'<<"20!= "<< factorial_20<<'\n'<<"30!= "<< factorial_30; 
+}
